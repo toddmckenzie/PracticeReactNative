0319eaace9d91246420aafc8abe1ceaefc232dce
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-12-22 22:28:00 -0600

    ability to drag aroudn the individual card

--- practice-advanced-topics-course/src/Deck.js
+++ practice-advanced-topics-course/src/Deck.js
@@ -6,23 +6,36 @@ import { Card, Button } from 'react-native-elements'
 class Deck extends React.Component {
     constructor(props){
         super(props);
-        this.state = {}
+        
+        const position = new Animated.ValueXY();
+        const panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (event, gesture) => {
+                position.setValue({ x: gesture.dx, y: gesture.dy })
+            },
+            onPanResponderRelease: () => {}
+        })
+
+        this.state = { panResponder, position };
     }
+
 
     render() {
     return (
-        <Card
-        // key={this.props.items.id}
-        title={this.props.item.text}
-        image={{ uri: this.props.item.uri }}
-        >
-        <Text style={{ marginBottom: 10 }}> Just more customizations </Text>
-        <Button
-            icon={{ name: 'code' }}
-            backgroundColor="#03A9F4"
-            title="View Now!"
-        />
-        </Card>
+        <Animated.View style={this.state.position.getLayout()} {...this.state.panResponder.panHandlers}>
+            <Card
+            // key={this.props.items.id}
+            title={this.props.item.text}
+            image={{ uri: this.props.item.uri }}
+            >
+            <Text style={{ marginBottom: 10 }}> Just more customizations </Text>
+            <Button
+                icon={{ name: 'code' }}z
+                backgroundColor="#03A9F4"
+                title="View Now!"
+            />
+            </Card>
+        </Animated.View>
     )
     }
 }
