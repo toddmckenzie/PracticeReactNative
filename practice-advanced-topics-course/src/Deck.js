a29877498997ba68bd69193fdfa915030083f1c8
Todd McKenzie <toddmckenzie@hotmail.com>
2020-04-09 15:10:36 -0500

    card rotating on drag

--- practice-advanced-topics-course/src/Deck.js
+++ practice-advanced-topics-course/src/Deck.js
@@ -17,12 +17,28 @@ class Deck extends React.Component {
         })
 
         this.state = { panResponder, position };
+        
+      
+
     }
 
+      getCardStyle = () => {
+          const { position } = this.state;
+          const rotate = position.x.interpolate({
+              inputRange:  [-500, 0, 500],
+              outputRange: ['-120deg', '0deg', '120deg']
+          })
+          return {
+              ...position.getLayout(),
+              transform: [{ rotate }]
+          }
+        
+    }
+    
 
     render() {
     return (
-        <Animated.View style={this.state.position.getLayout()} {...this.state.panResponder.panHandlers}>
+        <Animated.View style={this.getCardStyle()} {...this.state.panResponder.panHandlers}>
             <Card
             title={this.props.item.text}
             image={{ uri: this.props.item.uri }}
