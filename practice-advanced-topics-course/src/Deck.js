1f1f902b20afd55e0280b76df8d702f98b6ae010
Todd McKenzie <toddmckenzie@hotmail.com>
2020-04-09 17:42:34 -0500

    changed rotation

--- practice-advanced-topics-course/src/Deck.js
+++ practice-advanced-topics-course/src/Deck.js
@@ -26,7 +26,7 @@ class Deck extends React.Component {
       getCardStyle = () => {
           const { position } = this.state;
           const rotate = position.x.interpolate({
-              inputRange:  [-500, 0, 500],
+              inputRange:  [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
               outputRange: ['-120deg', '0deg', '120deg']
           })
           return {
